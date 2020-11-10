e29ee3d8bb27936258ab5cd9139c3e3591ecd995
Matt Grimmett <mgrimmett76@gmail.com>
2020-11-10 12:54:54 -0600

    account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+using API.Data;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context){
+
+            _context = context;
+        }
+    }
+}
