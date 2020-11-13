4d6e4a3eca52a04b9d429c90b180264908921e50
Matt Grimmett <mgrimmett76@gmail.com>
2020-11-13 13:32:59 -0600

    added basic login, cleaned up db

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -24,7 +24,7 @@ namespace API.Controllers
 
 
             if(await UserExists(registerDto.Username)) return BadRequest("Username is taken.");
-            
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
@@ -41,7 +41,25 @@ namespace API.Controllers
         }
 
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if(user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password.");
+            }
+
+                return null;
+        }
+
         private async Task<bool> UserExists(string username) {
+
+            //making sure the user doesn't already exist before creating it
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
